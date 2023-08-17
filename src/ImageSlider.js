f7054e6b84378faa5b3ea51b2245c36fe15c132b
Emilia_Majdys <majdysola@gmail.com>
2023-08-17 18:00:37 +0200

    added files and axios for images fetch

---
+++ src/ImageSlider.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import axios from 'axios';
+import { FaAngleLeft, FaAngelRight} from 'react-icons/fa';
+import './ImageSlider.css'
+
+
+
+const ImageSlider = () => {
+    return(
+        <div></div>
+    );
+    
+}
+
+export default ImageSlider;
